32a3e2420db0b7baa2288eaaa29b70d9d3341c98
TechnoShark13 <mathieu.dasilva13@gmail.com>
2019-10-16 08:57:58 +0200

    Ajout du calcul de temps d'éxécution

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,12 @@ public class Main {
 
     public static void main(String[] args){
 
+        long start = System.nanoTime();
+        String filepath = "C:\\Users\\Utilisateur\\Documents\\Cours L3 informatique\\Algorithmique 2\\TP\\TP2\\dico.txt";
 
+        Dico dico = DicoParser.parseFileToDico(filepath);
+        System.out.println(dico.contains("banane"));
+
+        System.out.println(String.format("Temps écoulé : %.2f ms", (System.nanoTime() - start)/1e6));
     }
 }
